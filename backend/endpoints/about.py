fd59505ed19559a5a1c5f77174c18b0211e8133c
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-11 21:47:48 +0100

    typo fix

--- backend/endpoints/about.py
+++ backend/endpoints/about.py
@@ -11,7 +11,7 @@ router = APIRouter(
 
 data={
 'title':'Who am I?',
-'description':'I am kurac freelance, self-educated, highly motivated, python developer from Serbia and I’ve been coding for 1 year with multiple years of supporting education. I usually work on building web scrapers, automation tools,FastAPI or Flask web apps. All kinds of automation, as well as sending emails or working with excel files. My primary concentration at the moment is python and have a firm understanding of HTML, CSS and Javascipt. I am comfortable with working on both Windows and Linux operating systems. To help enhance and support my graphics projects I often work with GIMP and Inkscape.'
+'description':'I am freelance, self-educated, highly motivated, python developer from Serbia and I’ve been coding for 1 year with multiple years of supporting education. I usually work on building web scrapers, automation tools,FastAPI or Flask web apps. All kinds of automation, as well as sending emails or working with excel files. My primary concentration at the moment is python and have a firm understanding of HTML, CSS and Javascipt. I am comfortable with working on both Windows and Linux operating systems. To help enhance and support my graphics projects I often work with GIMP and Inkscape.'
 }
 @router.get("/")
 def about_me(request: Request):
